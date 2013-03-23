c4b10375ad8e62e82dd780996b9020c305cc6136
Taiki ONO <taiks.4559@gmail.com>
2013-03-24 00:03:04 +0900

    add update filter

--- mikutter-kanabun.rb
+++ mikutter-kanabun.rb
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 Plugin.create(:kanabun) do
-  str=["カナブン食べてる","最近カナブン食いまくってる気がする","気分悪いの、ずっとカナブン食べてるからかもしれん。もう食べない。","そう言えば今日はおやつにカナブン食べてしまったな","親にTwitter見られてるの忘れててツイートしてたけどカナブン食べてるのバレてしまった","カナブンを…喰ってる…！？"]
-  command(:kanabun,
-          name: "カナブンを食べる",
-          condition: lambda{|opt| true},
-          visible: false,
-          role: :window ) do |opt|
+
+  kanabuns = ["カナブン食べてる","最近カナブン食いまくってる気がする","気分悪いの、ずっとカナブン食べてるからかもしれん。もう食べない。","そう言えば今日はおやつにカナブン食べてしまったな","親にTwitter見られてるの忘れててツイートしてたけどカナブン食べてるのバレてしまった","カナブンを…喰ってる…！？"]
+
+  command(
+    :kanabun,
+    name: "カナブンを食べる",
+    condition: lambda{|_| true},
+    visible: false,
+    role: :window
+  ) do |opt|
     Thread.new {  
-      Post.primary_service.update(:message => str[rand(str.size)])
+      Post.primary_service.update(:message => kanabuns.sample)
     }
+  end
+
+  filter_update do |service, msgs|
+    [
+      service,
+      msgs.map do |msg|
+        msg[:message] = msg[:message].gsub /[ァ-ヴ]+/u, "カナブン" if msg && msg[:message].respond_to?(:gsub)
+        msg
+      end
+    ]
   end
 end
